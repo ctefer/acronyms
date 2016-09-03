1e40c26a41ffd5b6ed5da02833fbde920372efd8
Chris Tefer <ctefer@gmail.com>
2016-09-03 01:16:17 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Acronyms',
+      version='0.01',
+      description='Acronyms Library',
+      author='Chris Tefer',
+      author_email='ctefer@gmail.com',
+      url='https://ctefer.github.io',
+      packages=['acronyms'],
+     )
+
+     
